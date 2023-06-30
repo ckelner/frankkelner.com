082b166b4c5d5c8f4f5ffcdd5d90e0e4daf40f4a
Chris Kelner <chris.kelner@gmail.com>
2023-06-30 14:11:14 -0400

    no table

--- create-table-img.py
+++ create-table-img.py
@@ -4,28 +4,17 @@ import os
 
 fileout = open("html-table.html", "w")
 
-table = "<table>\n"
+html = ""
 # specify the img directory path
 path = "/Users/chris.kelner/Downloads/pop-pop"
 
 # list files in img directory
 files = os.listdir(path)
-count = 0
 
 for file in files:
-    if count == 0:
-        table += "  <tr>\n"
-    table += '    <td><img src="https://pop-pop.s3.us-east-1.amazonaws.com/{0}"/></td>\n'.format(
+    html += '<img src="https://pop-pop.s3.us-east-1.amazonaws.com/{0}"/><br/>\n'.format(
         file
     )
-    if count == 2:
-        table += "  </tr>\n"
-        count = 0
-    else:
-        count += 1
 
-table += "  </tr>\n"
-table += "</table>"
-
-fileout.writelines(table)
+fileout.writelines(html)
 fileout.close()
